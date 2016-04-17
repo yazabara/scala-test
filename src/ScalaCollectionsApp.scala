d0434f2f4bdcffa6431a5353130fba2b70848e1c
Ярослав Забара < >
2016-04-17 21:22:00 +0300

    collections added

---
+++ src/ScalaCollectionsApp.scala
@@ -0,0 +1,17 @@
+/**
+  * @author Yaroslav Zabara
+  */
+object ScalaCollectionsApp {
+  def main(args: Array[String]) = {
+    val numbers = List(1, 2, 3, 4)
+    println(numbers)
+    //
+    val set1 =  Set(1, 1, 2)
+    println(set1)
+    //
+    val hostPort = ("localhost", 80)
+    println(hostPort._1)
+    println(hostPort._2)
+    println(hostPort)
+  }
+}
